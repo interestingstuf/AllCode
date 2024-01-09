59d3edee4b2a8562b8180f75842c55759e5a4979
overedits7 <111271071+interestingstuf@users.noreply.github.com>
2024-01-08 20:39:07 -0800

    insane update

--- DayRoutine/dayroutine.py
+++ DayRoutine/dayroutine.py
@@ -1,34 +1,48 @@
 import tkinter as tk
 from tkinter import filedialog
 import webbrowser as wb
-import os
+import os 
 import subprocess
-def f1():
+
+import customtkinter as ctk
+def fileopen():
     filename = filedialog.askopenfilename()
     print(filename)
     subprocess.Popen([filename  ,"sudo /usr/bin/open Preview"],shell=True)
     
+def mainmail():  
+    wb.open('https://mail.google.com/mail/u/1/#inbox')
 
-def f2():  
-    wb.open('https://meet.google.com/pnh-wttc-ahh')
-
-def f3():
+def piano():
     wb.open('https://us02web.zoom.us/j/2320221486?pwd=SWJYMjl0aVZRNW1rMUoxcElwaW1hUT09#success')
 
+
+    
 root = tk.Tk()
-root.title("Day Routine Manager")
+root.title("Manager")
 root.geometry("500x500")
 menubar=tk.Menu(root)
 #FILE BAR
 filemenu=tk.Menu(menubar,background="#A5D0FD", activebackground="#F59C9C")
-filemenu.add_command(label="Open File", command=f1)
+filemenu.add_command(label="Open File", command=fileopen)
 menubar.add_cascade(label="File",menu=filemenu)
 
 #CLASSES BAR
 classmenu=tk.Menu(menubar)
-classmenu.add_command(label="Coding Class",command=f2)
-classmenu.add_command(label="Piano Class",command=f3)
+classmenu.add_command(label="Piano Class",command=piano)
 menubar.add_cascade(label="Classes",menu=classmenu)
+
+#SHORTCUTS BAR
+shortcutmenu=tk.Menu(menubar)
+shortcutmenu.add_command(label="Main Mail",command=mainmail)
+menubar.add_cascade(label="Shortcuts",menu=shortcutmenu)
+
+#ON-SCREEN
+
+timelabel=ctk.CTkLabel(root,text="Made by: Parth Amradkar",text_color="#323b3b")
+timelabel.place(x=200,y=475)
+
+
 #CONFIG
 root.config(menu=menubar, background="#D4F3FC")
 root.mainloop()
--- Inspirit/Day 2/code.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn import metrics
-
-data = pd.read_csv('cancer.csv')
-data['diagnosis'].replace({'M':1, 'B':0}, inplace = True)
-data.to_csv('cancer.csv')
-del data
